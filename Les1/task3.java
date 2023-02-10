8baefa9fa389fb6178209204a5f86374e9d7480e
Bad_S <Evlampyev.Alex@gmail.com>
2023-02-10 20:00:36 +0500

    Begin task 3

--- Les1/task3.java
+++ Les1/task3.java
@@ -5,7 +5,12 @@ public class task3 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         String example = scan.nextLine();
-        
-        
-    }    
+        scan.close();
+
+                
+    }   
+    
+    public static int multiplication(text: String) {
+        System.out.println(text);
+    }
 }
