b43cc7de981c3c4549a0387cd3663c4e3f02fdcb
Bad_S <Evlampyev.Alex@gmail.com>
2023-02-15 19:41:55 +0500

    HomeWork ended

--- Les2/Task2a.java
+++ Les2/Task2a.java
@@ -26,9 +26,24 @@ public class Task2a {
         BufferedReader bufReader = new BufferedReader(new FileReader(file));
         Line = bufReader.readLine();
         bufReader.close();
-        System.out.println(Line);
         Path path = Paths.get(file_name);
-        List <String> Lines = Files.readAllLines(path,StandardCharsets.UTF_8);
-        Lines.stream().forEach(System.out::println);
+        List<String> Lines = Files.readAllLines(path, StandardCharsets.UTF_8);
+        for (String OneLine : Lines) {
+            String[] lst = OneLine.split(",");
+            String family = onlyTxt(lst[0]);
+            String mark = onlyTxt(lst[1]);
+            String object = onlyTxt(lst[2]);
+            String rezult = "У студента " + family + " по предмету " + object + " " + mark;
+            System.out.println(rezult);
+            rezult = "";
+
+        }
+
+    }
+
+    public static String onlyTxt(String text) {
+        int index = text.lastIndexOf(':');
+        String res = text.substring(index + 2, text.length() - 1);
+        return res;
     }
 }
